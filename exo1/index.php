27863e7a44d2532443cfea696dfa4d5de2650dd3
CarTig <carolle.t.n@gmail.com>
2022-12-22 14:31:31 +0100

    creation foumulaire exo1

---
+++ exo1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo1</title>
+</head>
+<body>
+    <form method="post" action="">
+        <p>
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom">
+        </p>
+        <p>
+            <label for="prenom">Prénom : </label>
+            <input type="text" name="prenom" id="prenom">
+        </p>
+        <p>
+            <input type="submit" value="Valider">
+        </p>
+
+    </form>
+</body>
+</html>
